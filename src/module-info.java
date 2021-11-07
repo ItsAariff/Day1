8bfeb4ae335392a5ffa6d634d75f28ac0249a003
Aarif Chaudhary <Aarif Chaudhary@Aarif>
2021-11-07 23:36:03 +0530

    Version 1

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GFTraining1 {
+}
---
+++ src/pkg1/Hello.java
@@ -0,0 +1,11 @@
+package pkg1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Arif Chaudhary(Batch 5)");
+
+
+	}
+
+}
