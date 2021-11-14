1226bde4799361676674f5175abad641e2dd8999
Aarif Chaudhary <Aarif Chaudhary@Aarif>
2021-11-14 21:37:32 +0530

    Pushing

--- src/pkg1/Hello.java
+++ src/pkg1/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Arif Chaudhary(Batch 5)");
+		System.out.println("Hello from Ann (Batch 5)");
 
 
 	}
